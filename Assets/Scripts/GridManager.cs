bc5e512047a7804337ad118e6158f8266fe26632
chi <ma.duongchi@gmail.com>
2022-02-01 21:42:43 -0500

    player is able to create a grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
- 
+    [SerializeField]
+    private int gridWidth, gridHeight;
+    [SerializeField]
+    private Tile tilePrefab;
+    [SerializeField]
+    private Transform camera;
+
+    public void GenerateGrid()
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+        }
+
+        camera.transform.position = new Vector3(gridWidth / 2.0f - 0.5f, gridHeight / 2.0f - 0.5f, -10);
+    }
 }
