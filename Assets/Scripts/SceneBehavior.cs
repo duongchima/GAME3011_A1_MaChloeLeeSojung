20ab7b43edb36a0bed64594915c1719708f12e7c
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-02-03 15:28:03 -0500

    Added GUI Assets & Fade Animations
    - Added GUI assets from Unity Store
    - Added Fade Animations in Start and Main Scene

---
+++ Assets/Scripts/SceneBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneBehavior : MonoBehaviour
+{
+    public Animator animController;
+    public void NextScene(string scene)
+    {
+        animController.enabled = true;
+        StartCoroutine(WaitforScene(1.0f, scene));
+    }
+
+    IEnumerator WaitforScene(float waitTime, string sceneName)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
