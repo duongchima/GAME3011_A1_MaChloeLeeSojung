69ed4c23a2cd22d2b5c5cb8f1dc974e0060c2b02
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-02-03 16:28:57 -0500

    Added Resource Counter UI (No Functionality) and Updated UI for Changing Modes
    Used new GUI assets to update mode buttons and added resource counter UI

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject ResourcePanel;
+
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => SetActive());
+    }
+
+    public void SetActive()
+    {
+        ResourcePanel.gameObject.SetActive(!ResourcePanel.activeSelf);
+    }
+}
