f2d225e52e2c35419381c74d55a8f42a56197451
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-02-03 03:06:27 -0500

    Updated Grid Generator and Tile GameObject
    - UPDATED: Grid Generator updated to create max, half, and quarter tiles
    - UPDATED: Tile properties updated in Tile class script
    - ADDED: Tile type enum to organize tiles
    - Able to create tile buttons properly onto tilemap

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,23 +5,100 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField]
-    private int gridWidth, gridHeight;
+    private float gridWidth, gridHeight;
     [SerializeField]
-    private Tile tilePrefab;
+    private Tile tilePrefab = null;
     [SerializeField]
-    private Transform camera;
+    private Canvas canvas = null;
+    [SerializeField]
+    private int NumOfMaximumTiles = 0;
+
+    private List<Tile> tilesList = new List<Tile>();
+
+    private void Start()
+    {
+        Random.seed = 42;
+    }
+
+    private void ClearGrid()
+    {
+        if (tilesList.Count != 0) tilesList.Clear();
+        if (canvas.transform.childCount > 0)
+        {
+            foreach (Transform child in canvas.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
 
     public void GenerateGrid()
     {
-        for (int x = 0; x < gridWidth; x++)
+        ClearGrid();
+
+        Vector2 canvasSize = canvas.GetComponent<RectTransform>().rect.position;
+        Rect tileSize = tilePrefab.GetComponent<RectTransform>().rect;
+
+        for (float x = 0; x < gridWidth; x++)
         {
-            for (int y = 0; y < gridHeight; y++)
+            for (float y = 0; y < gridHeight; y++)
             {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(((x - gridWidth / 2) * tileSize.width) - canvasSize.x, ((y - gridWidth / 2) * tileSize.height) - canvasSize.y), Quaternion.identity, canvas.transform);
                 spawnedTile.name = $"Tile {x} {y}";
+                spawnedTile.position = new Vector2(spawnedTile.position.x, spawnedTile.position.y);
+                spawnedTile.id = new Vector2(x, y);
+                spawnedTile.type = TileType.QUARTER;
+                tilesList.Add(spawnedTile);
             }
         }
 
-        camera.transform.position = new Vector3(gridWidth / 2.0f - 0.5f, gridHeight / 2.0f - 0.5f, -10);
+        GenerateMaximumTiles();
+        foreach (Tile tile in tilesList)
+        {
+            if (tile.type.Equals(TileType.MAXIMUM)) GenerateHalfTiles(tile);
+        }
+        GenerateQuarterTiles();
+    }
+
+    public void GenerateMaximumTiles()
+    {
+        int randomTileX, randomTileY; 
+        for (int i = 0; i < NumOfMaximumTiles; i++)
+        {
+            randomTileX = (int)Random.Range(0, gridWidth);
+            randomTileY = (int)Random.Range(0, gridHeight);
+
+            foreach (Tile tile in tilesList)
+            {
+                if (tile.GetId().Equals(new Vector2(randomTileX, randomTileY)))
+                {
+                    if (tile.type.Equals(TileType.MAXIMUM)) i--;
+                    else tile.SetMaxTile(new Vector2(randomTileX, randomTileY));
+                }
+            }
+        }
+    }
+
+    public void GenerateHalfTiles(Tile maxTile)
+    {
+        foreach(Tile tile in tilesList)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                tile.SetHalfTile(new Vector2(maxTile.GetId().x - 1, maxTile.GetId().y + j));
+                tile.SetHalfTile(new Vector2(maxTile.GetId().x + 1, maxTile.GetId().y + j));
+            }
+
+            tile.SetHalfTile(new Vector2(maxTile.GetId().x, maxTile.GetId().y - 1));
+            tile.SetHalfTile(new Vector2(maxTile.GetId().x, maxTile.GetId().y + 1));
+        }
+    }
+
+    public void GenerateQuarterTiles()
+    {
+        foreach(Tile tile in tilesList)
+        {
+            tile.SetQuarterTile();
+        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,8 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
+    public Vector2 id;
+    public Vector2 position;
 
+    public TileType type;
+
+    public void SetPosition(int x, int y)
+    {
+        position.x = x;
+        position.y = y;
+    }
+
+    public Vector2 GetId()
+    {
+        Debug.Log(id.x + "," + id.y);
+        return id;
+    }
+
+    public void SetMaxTile(Vector2 maxId)
+    {
+        if (id.Equals(maxId))
+        {
+            type = TileType.MAXIMUM;
+            this.gameObject.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    public void SetHalfTile(Vector2 maxId)
+    {
+        if (id.Equals(maxId))
+        {
+            type = TileType.HALF;
+            this.gameObject.GetComponent<Image>().color = Color.magenta;
+        }
+    }
+    public void SetQuarterTile()
+    {
+        if (!type.Equals(TileType.MAXIMUM) && !type.Equals(TileType.HALF))
+        {
+            type = TileType.QUARTER;
+            this.gameObject.GetComponent<Image>().color = Color.yellow;
+        }
+    }
 }
